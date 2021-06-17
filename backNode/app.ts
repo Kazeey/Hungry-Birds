3eef219a234831e3cfeffa198643af3da14e9d38
Thomas.D <thomlgt@hotmail.fr>
2021-06-17 11:47:54 +0200

    Ajout message erreur connexion BDD

--- backNode/app.ts
+++ backNode/app.ts
@@ -4,7 +4,13 @@ import { connexionSQL } from './config/mysql.config';
 const app = express();
 const port = 3001;
 
-connexionSQL.connect();
+connexionSQL.connect(error => {
+    if(error) {
+        throw error;
+    } else {
+        console.log("Connecté à la base de données Hungry Birds");
+    }
+});
 
 app.listen(port, () => {
     console.log(`Connected | Port : ${port}`)
