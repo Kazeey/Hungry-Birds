f2d2bef9b8eb80b47bf26408853d7b414f9fcd86
Quentin Pezé <quentin@aloatec.local>
2021-09-07 15:53:01 +0200

    Ajout page connexion
    TODO : Problem change on null

---
+++ frontJava/app/src/main/java/com/example/hungrybirds/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.hungrybirds;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void changeMessage(View view)
+    {
+        ((TextView)findViewById(R.id.connexionMessage)).setText(R.string.app_name);
+    }
+}
