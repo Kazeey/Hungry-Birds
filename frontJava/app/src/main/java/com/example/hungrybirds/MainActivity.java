e2f4079c2938f1254ad0e8b020b9dce22e3a95dd
Kazeey <44819132+Kazeey@users.noreply.github.com>
2021-09-08 23:33:35 +0200

    update

--- frontJava/app/src/main/java/com/example/hungrybirds/MainActivity.java
+++ frontJava/app/src/main/java/com/example/hungrybirds/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.hungrybirds;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
