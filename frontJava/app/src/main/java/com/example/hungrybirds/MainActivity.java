f03946c0280e4584076edc6e288f3c1b125d51ac
Quentin Pezé <quentin@aloatec.local>
2021-09-09 11:40:03 +0200

    Tentative ajout nouvelle activité

--- frontJava/app/src/main/java/com/example/hungrybirds/MainActivity.java
+++ frontJava/app/src/main/java/com/example/hungrybirds/MainActivity.java
@@ -1,20 +1,40 @@
 package com.example.hungrybirds;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "Bonjour";
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    public void changeMessage(View view)
-    {
-        ((TextView)findViewById(R.id.connexionMessage)).setText(R.string.app_name);
+        Button button = findViewById(R.id.connexionButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view)
+            {
+                System.out.println("test");
+            }
+        });
     }
+/*
+    public void changeActivity(View view)
+    {
+        Intent intent = new Intent(this, GestionUser.class);
+        EditText editTextMail = (EditText) findViewById(R.id.editConnectionMailAddress);
+        EditText editTextPassword = (EditText) findViewById(R.id.editConnectionPassword);
+        String message = editTextMail.getText().toString() + " " + editTextPassword.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+*/
 }
