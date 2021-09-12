15417bf2feeeec133ab1e0fdf9d68b54b648f7f2
unknown <you@example.com>
2021-09-12 15:29:41 +0200

    Kotlin -> Java

---
+++ frontJava/app/src/main/java/com/example/hungrybirds/NotConnectedActivity.java
@@ -0,0 +1,15 @@
+package com.example.hungrybirds;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NotConnectedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_not_connected);
+
+    }
+}
--- frontJava/app/src/main/java/com/example/hungrybirds/ShoppigCartActivity.kt
+++
@@ -1,4 +0,0 @@
-package com.example.hungrybirds
-
-class ShoppigCartActivity {
-}
